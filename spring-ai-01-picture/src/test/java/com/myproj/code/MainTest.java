67d9f3b6f2ea1635c5e0b37918effda739cdd3e2
aobaba <xxxx@qq.com>
2026-08-18 16:39:58 +0800

    feat: 图像识别测试

--- spring-ai-01-picture/src/test/java/com/myproj/code/MainTest.java
+++ spring-ai-01-picture/src/test/java/com/myproj/code/MainTest.java
@@ -5,19 +5,29 @@ import com.alibaba.dashscope.aigc.multimodalconversation.MultiModalConversationP
 import com.alibaba.dashscope.aigc.multimodalconversation.MultiModalConversationResult;
 import com.alibaba.dashscope.common.MultiModalMessage;
 import com.alibaba.dashscope.common.Role;
+import com.alibaba.dashscope.exception.NoApiKeyException;
+import com.alibaba.dashscope.exception.UploadFileException;
 import com.alibaba.dashscope.utils.Constants;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest(classes = StartApp.class)
 public class MainTest {
 
+    static {
+        Constants.baseHttpApiUrl = "https://ws-2o9uw28fguatnoo9.cn-beijing.maas.aliyuncs.com/api/v1";
+    }
+    @Value("${spring.ai.dashscope.api-key}")
+    private String apiKey;
     @Test
     public void test01(){
-        Constants.baseHttpApiUrl = "https://ws-2o9uw28fguatnoo9.cn-beijing.maas.aliyuncs.com/api/v1";
+
 
         // [方法一] 使用公网图像URL
         String imageUrl = "https://alidocs.oss-cn-zhangjiakou.aliyuncs.com/res/yBRq1ZPYEaXdyOdv/img/33a80a19-7ac7-4c64-b0fa-7d685b7046a0.png";
@@ -50,8 +60,6 @@ public class MainTest {
     }
     @Test
     public void test02(){
-        Constants.baseHttpApiUrl = "https://ws-2o9uw28fguatnoo9.cn-beijing.maas.aliyuncs.com/api/v1";
-
         MultiModalConversation conv = new MultiModalConversation();
         MultiModalMessage userMessage = MultiModalMessage.builder()
                 .role(Role.USER.getValue())
@@ -75,4 +83,63 @@ public class MainTest {
         }
     }
 
+
+    @Test
+    public void test03() throws NoApiKeyException, UploadFileException {
+        MultiModalConversation conv = new MultiModalConversation();
+        MultiModalMessage userMessage = MultiModalMessage.builder().role(Role.USER.getValue())
+                .content(Arrays.asList(
+                        Collections.singletonMap("image", "https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20241022/emyrja/dog_and_girl.jpeg"),
+                        Collections.singletonMap("text", "图中描绘的是什么景象?"))).build();
+        MultiModalConversationParam param = MultiModalConversationParam.builder()
+                // 若没有配置环境变量，请用百炼API Key将下行替换为：.apiKey("sk-xxx")
+                // 各地域的API Key不同。获取API Key：https://help.aliyun.com/zh/model-studio/get-api-key
+                .apiKey(apiKey)
+                .model("qwen3.8-max")  // 此处以qwen3.8-max为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/models
+                .messages(Arrays.asList(userMessage))
+                .build();
+        MultiModalConversationResult result = conv.call(param);
+        System.out.println(result.getOutput().getChoices().get(0).getMessage().getContent().get(0).get("text"));
+    }
+
+    @Test
+    public void test04() throws NoApiKeyException, UploadFileException {
+        MultiModalConversation conv = new MultiModalConversation();
+        MultiModalMessage userMessage = MultiModalMessage.builder().role(Role.USER.getValue())
+                .content(Arrays.asList(
+                        Collections.singletonMap("image", "https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20241022/emyrja/dog_and_girl.jpeg"),
+                        Collections.singletonMap("image", "https://dashscope.oss-cn-beijing.aliyuncs.com/images/tiger.png"),
+                        Collections.singletonMap("text", "这些图描绘了什么内容?"))).build();
+        MultiModalConversationParam param = MultiModalConversationParam.builder()
+                // 若没有配置环境变量，请用百炼API Key将下行替换为：.apiKey("sk-xxx")
+                // 各地域的API Key不同。获取API Key：https://help.aliyun.com/zh/model-studio/get-api-key
+                .apiKey(apiKey)
+                .model("qwen3.8-max")  // 此处以qwen3.8-max为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/models
+                .messages(Arrays.asList(userMessage))
+                .build();
+        MultiModalConversationResult result = conv.call(param);
+        System.out.println(result.getOutput().getChoices().get(0).getMessage().getContent().get(0).get("text"));
+    }
+
+    @Test
+    public void test05() throws NoApiKeyException, UploadFileException {
+        MultiModalConversation conv = new MultiModalConversation();
+        // fps 可参数控制视频抽帧频率，表示每隔 1/fps 秒抽取一帧，完整用法请参见：https://help.aliyun.com/zh/model-studio/use-qwen-by-calling-api?#2ed5ee7377fum
+        Map<String, Object> params = new HashMap<>();
+        params.put("video", "https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20241115/cqqkru/1.mp4");
+        params.put("fps", 2);
+        MultiModalMessage userMessage = MultiModalMessage.builder().role(Role.USER.getValue())
+                .content(Arrays.asList(
+                        params,
+                        Collections.singletonMap("text", "这段视频的内容是什么?"))).build();
+        MultiModalConversationParam param = MultiModalConversationParam.builder()
+                // 若没有配置环境变量，请用百炼API Key将下行替换为：.apiKey("sk-xxx")
+                // 各地域的API Key不同。获取API Key：https://help.aliyun.com/zh/model-studio/get-api-key
+                .apiKey(apiKey)
+                .model("qwen3.8-max")
+                .messages(Arrays.asList(userMessage))
+                .build();
+        MultiModalConversationResult result = conv.call(param);
+        System.out.println(result.getOutput().getChoices().get(0).getMessage().getContent().get(0).get("text"));
+    }
 }
